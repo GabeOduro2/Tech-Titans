fd3f8235f8285a49cf493b5d67f7cf25c337334d
hudso2tj <hudso2tj@dukes.jmu.edu>
2024-04-02 12:55:52 -0400

    Collab changes

---
+++ Lab3/Pages/Collaboration/Budgeting.cshtml.cs
@@ -0,0 +1,17 @@
+using Lab3.Pages.DataClasses;
+using Lab3.Pages.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+
+namespace Lab3.Pages.Collaboration
+{
+    public class BudgetingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Lab3/Pages/Collaboration/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3.Pages.Collaboration
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
