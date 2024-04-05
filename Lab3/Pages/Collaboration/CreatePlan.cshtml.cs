5ce5eb3fe4cf8ed7190558e41560d004edbcfb65
hudso2tj@dukes.jmu.edu <hudso2tj@dukes.jmu.edu>
2024-04-05 14:25:44 -0400

    Code Cleanup, unused methods/pages deleted

--- Lab3/Pages/Collaboration/CreatePlan.cshtml.cs
+++ Lab3/Pages/Collaboration/CreatePlan.cshtml.cs
@@ -8,19 +8,12 @@ using System.Data.SqlClient;
 namespace Lab3.Pages.Collaboration
 {
     public class CreatePlanModel : PageModel
-    {
-        [BindProperty] public Plan NewPlan { get; set; }
-        [BindProperty] public int NumSteps { get; set; }
-        [BindProperty] public int SelectedUsers { get; set; }
-        [BindProperty(SupportsGet = true)] public List<SelectListItem> Users { get; set; }
-        [BindProperty] public string? Message { get; set; }
+    { 
 
         public IActionResult OnGet(int Steps)
         {
             if (HttpContext.Session.GetString("username") != null)
             {
-                // Display page
-                LoadUsers();
                 return Page();
             }
             else
@@ -31,34 +24,6 @@ namespace Lab3.Pages.Collaboration
             }
         }
 
-        public IActionResult OnPostSave()
-        {
-            if (NewPlan.Name != null)
-            {
-                DBClass.InsertNewPlan(NewPlan);
-                Message = NewPlan.Name + " has been created";
-            }
-            LoadUsers();
-            return Page();
-        }
 
-        public void LoadUsers()
-        {
-            Users = new List<SelectListItem>();
-
-            // Populating User SELECT control
-            SqlDataReader UserReader = DBClass.GeneralReaderQuery("SELECT * FROM [USER]");
-
-
-            while (UserReader.Read())
-            {
-                string fullName = $"{UserReader["FirstName"]} {UserReader["LastName"]}";
-                Users.Add(
-                    new SelectListItem(
-                        fullName,
-                        UserReader["UserID"].ToString()));
-            }
-            DBClass.Lab3DBConnection.Close();
-        }
     }
 }
