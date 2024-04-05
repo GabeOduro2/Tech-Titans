fa3fd1cf23741fd15857e0e9921f607327762364
hudso2tj@dukes.jmu.edu <hudso2tj@dukes.jmu.edu>
2024-04-05 00:25:19 -0400

    Fixed "files" tab uploading functionality

--- Lab3/Pages/FileHandling.cshtml.cs
+++ Lab3/Pages/FileHandling.cshtml.cs
@@ -8,5 +8,21 @@ namespace Lab3.Pages
         public void OnGet()
         {
         }
+
+        public IActionResult OnPostUpload(IFormFile fileupload)
+        {
+            if (fileupload != null && fileupload.Length > 0)
+            {
+                string uploadsDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "fileupload");
+                string filePath = Path.Combine(uploadsDir, fileupload.FileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    fileupload.CopyTo(fileStream);
+                }
+
+                return RedirectToPage();
+            }
+            return Page();
+        }
     }
 }
